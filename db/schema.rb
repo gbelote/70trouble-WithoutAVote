8baabf5eeba6e5551e6d89099df7ffe6bda57f0e
Greg Belote <gbelote@gbelote.local>
2010-07-30 14:47:48 -0400

    added vote model

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100730184352) do
+ActiveRecord::Schema.define(:version => 20100730184535) do
 
   create_table "polls", :force => true do |t|
     t.string   "title"
     t.text     "desc"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "poll_id"
+    t.string   "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
